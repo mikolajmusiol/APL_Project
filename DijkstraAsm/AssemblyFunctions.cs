a3b77a79802164a95e8260a1e17e167efc0ba78c
mikolajmusiol <mm306074@student.polsl.pl>
2024-12-02 14:34:57 +0100

    Git config

---
+++ DijkstraAsm/AssemblyFunctions.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DijkstraAsm;
+
+public static class AssemblyFunctions
+{
+    [DllImport("../../../../../x64/Debug/JADll.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int AddTwoNumbers(int a, int b);
+}
--- DijkstraAsm/Form1.cs
+++ DijkstraAsm/Form1.cs
@@ -1,23 +1,16 @@
-using System.Runtime.InteropServices;
-
 namespace DijkstraAsm
 {
     public partial class Form1 : Form
     {
-
-        // Import a function from the DLL
-        [DllImport("JADll.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int AddTwoNumbers(int a, int b);
-
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Example usage of the function
-            int result = AddTwoNumbers(1,2);
+            int result = AssemblyFunctions.AddTwoNumbers(1,2);
             MessageBox.Show($"Result: {result}");
 
         }
