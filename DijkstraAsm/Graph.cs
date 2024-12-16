87ed40e6b1be44278bf3ce25b9da483844d63792
Mikołaj Musioł <96127814+mikolajmusiol@users.noreply.github.com>
2024-12-16 13:56:22 +0100

    Input reader (#3)
    Co-authored-by: Jokerr14 <pk306061@student.polsl.pl>

---
+++ DijkstraAsm/Graph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DijkstraAsm;
+
+public record Connection(int IdNodeA, int IdNodeB, int Weight);
+
+public class Graph
+{
+    public int StartNode { get; private set; }
+    public int EndNode { get; private set; }
+    public List<Connection> Connections { get; private set; } = new List<Connection>();
+
+    public Graph(string inputPath)
+    {
+        using (var reader = new StreamReader(inputPath))
+        {
+            try
+            {
+                StartNode = int.Parse(reader.ReadLine());
+                EndNode = int.Parse(reader.ReadLine());
+            } catch { }
+            
+            Regex reg = new Regex(@"\d+");
+
+            string line;
+
+            while((line = reader.ReadLine()) != null)
+            {
+                var match = reg.Matches(line);
+
+                try
+                {
+                    Connections.Add(new Connection(
+                        int.Parse(match[0].Value), 
+                        int.Parse(match[1].Value), 
+                        int.Parse(match[2].Value)));
+                } catch { }
+                
+            }
+        };
+    }
+}
