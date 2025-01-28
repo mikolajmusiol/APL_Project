e8b546d02178f77c0bd189e8f6a116e4c14e65c2
Mikołaj Musioł <96127814+mikolajmusiol@users.noreply.github.com>
2025-01-28 23:38:01 +0100

    Asm (#7)
    * kill me pls
    
    * KURWAAAAAAAAA
    
    ---------
    
    Co-authored-by: Jokerr14 <pk306061@student.polsl.pl>

--- DijkstraAsm/AssemblyFunctions.cs
+++ DijkstraAsm/AssemblyFunctions.cs
@@ -5,5 +5,5 @@ namespace DijkstraAsm;
 public static class AssemblyFunctions
 {
     [DllImport("../../../../../x64/Debug/JADll.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int AddTwoNumbers(int a, int b);
+    public static extern Int64 MyProc1(Int64[] arr, int vert, int src, int end);
 }
--- JADll/mydll.asm
+++ JADll/mydll.asm
@@ -1,16 +1,151 @@
-;------------------------------------------------------------------------
-;.586
+.data
+dist DQ 1000 dup(2147483647) ;dist[V]
+sptSet DQ 1000 dup(0) ;sptSet[v]
+
+storeVert DQ 0
+storeVertM DQ 0
+storeArrAdd DQ 0
+storeSrc DQ 0
+storeGoal DQ 0
+
+u DQ 0
+
+minIdx DQ 0
+min DQ 0
+
 .code
-;------------------------------------------------------------------------
-; Function: AddTwoNumbers
-; Takes two 64-bit integers in RCX and RDX and returns their sum in RAX.
-;------------------------------------------------------------------------
 
-AddTwoNumbers proc
-    mov     rax, rcx        ; Move the first parameter (RCX) into RAX
-    add     rax, rdx        ; Add the second parameter (RDX) to RAX
-    ret                     ; Return, result is in RAX
-AddTwoNumbers endp
+MyProc1 PROC
 
-END                         ; No entry point
-;------------------------------------------------------------------------
+	;STORING ALL ARGUMENTS
+	mov [storeArrAdd], rcx ;save Array Address to memory
+	mov [storeVert], rdx ;save # of Vert to memory
+	mov [storeVertM], rdx
+	dec [storeVertM] ;save # of Vert - 1 to memory
+	dec r8
+	dec r9
+	mov [storeSrc], r8
+	mov [storeGoal], r9
+
+	mov rcx, [storeVert] ;move # of Vert to ecx (counter)
+	mov rax, 2147483647 ;mov 2147483647 to rax
+	mov r8, OFFSET dist ;move address of dist[] to rbx
+	
+	mov rdi, [storeSrc]
+	mov r11, 0
+	mov [r8+rdi*8], r11
+	
+	mov r9, OFFSET sptSet
+	mov rcx, 0
+
+	;r8 = dist, r9 = sptSet
+
+loop_main:
+
+	cmp rcx, [storeVertM]
+	je done
+
+
+	call MinDist
+
+	mov rdx, [storeVert]
+	mov r10, [storeArrAdd]
+
+	mov qword ptr [r9+rax*8], 1
+	mov rbx, 0
+	
+
+loop_nest:
+
+	cmp rbx, [storeVert]
+	je next_main
+	mov rax, [minIdx]
+
+	;conditional 1
+	cmp qword ptr [r9+rbx*8], 0
+	jne next_inner
+
+	;conditional 3
+	cmp qword ptr [r8+rax*8], 2147483647
+	je next_inner
+
+	;conditional 2
+	;r10 + (rax * rdx + rbx) * 8 
+	;graph[2][4]
+	;mov rbx, 4
+	;mov rax, 2
+	mov rdx, [storeVert]
+	mul rdx
+	add rax, rbx
+	mov r11, 8
+	mul r11
+	add rax, r10
+	cmp qword ptr [rax], 0
+	je next_inner
+
+	;conditional 4 
+	mov r11, [minIdx]
+	mov r12, [r8+r11*8] ; dist[u] in r12
+	add r12, [rax]
+
+	cmp r12, [r8+rbx*8]
+	jg next_inner
+
+	;If OK
+	mov [r8+rbx*8], r12
+
+
+next_inner:
+	inc rbx
+	jmp loop_nest
+
+next_main:
+	inc rcx
+	jmp loop_main
+
+done:
+	mov rax, r8
+	ret
+
+MyProc1 ENDP
+
+
+MinDist PROC
+	
+	mov [min], 2147483647
+	mov r11, [storeVert]
+	xor rax, rax
+	
+loop_mindist:
+	cmp rax, r11
+	je done_loop
+
+	;1. cond
+	mov rdx, OFFSET sptSet
+	mov r10, [rdx+rax*8]
+	cmp r10, 1
+	je loop_it
+
+	;2. cond
+	mov rdx, OFFSET dist
+	mov r10, [rdx+rax*8]
+	cmp r10, [min]
+	jg loop_it
+
+	;min = dist[v]
+	mov [min], r10
+
+	;min_index = v
+	mov [minIdx], rax
+
+loop_it:
+	inc rax
+	jmp loop_mindist
+
+done_loop:
+	mov rax, [minIdx]
+	ret
+
+MinDist ENDP
+
+END
